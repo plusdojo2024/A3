602f7a663468037565337108505c48c7d7b9335b
plusdojo2024-sudo <sudo-hirohisa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 16:49:06 +0900

    add TimeLogic

---
+++ src/logic/TimeLogic.java
@@ -0,0 +1,39 @@
+package logic;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeLogic {
+
+	//フォーマットを日本語の年月日と時刻にして現在時刻を返す
+	public String nowJp() {
+
+		LocalDateTime now = LocalDateTime.now();//現在時刻取得
+		//日本語フォーマットを指定
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒");
+		String date = now.format(f);
+
+		return date;
+	}
+
+	//カレンダーで使用されているフォーマットで現在時刻を返す
+	public String nowCalendar() {
+
+		LocalDateTime now = LocalDateTime.now();//現在時刻取得
+		//フォーマットを指定
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String date = now.format(f);
+
+		return date;
+	}
+
+	//一般的なフォーマットで時刻を切り捨ててStringで返す
+	public String nowNomalDay() {
+		LocalDateTime now = LocalDateTime.now();//現在時刻取得
+		//フォーマットを指定
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		String date = now.format(f);
+
+		return date;
+	}
+}
