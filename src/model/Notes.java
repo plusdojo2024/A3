09966114d3300998daf425d26579ffcf8a74932d
plusdojo2024-calvin <suhardi-calvin-martin-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 15:25:10 +0900

    a

--- src/model/Notes.java
+++ src/model/Notes.java
@@ -3,5 +3,19 @@ package model;
 import java.io.Serializable;
 
 public class Notes implements Serializable{
+	private String title;
+	private String memo;
 
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
 }
--- src/servlet/NoteServlet.java
+++ src/servlet/NoteServlet.java
@@ -1,33 +1,76 @@
 package servlet;
 
+import java.io.File;
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import logic.FileLogic;
+import model.Users;
+
 
 /**
  * Servlet implementation class NoteServlet
  */
 @WebServlet("/NoteServlet")
+@MultipartConfig(location = "C:/pleiades/workspace/A3/WebContent/tmp", maxFileSize = 1024 * 1024 * 10)
 public class NoteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 引継ぎノートページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/note.jsp");
+		dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	@SuppressWarnings("unused")
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String title = request.getParameter("title");
+		String memo = request.getParameter("memo");
+
+		FileLogic fL = new FileLogic();
+
+
+		HttpSession session = request.getSession();
+
+		Users user = (Users) session.getAttribute("user");
+
+	Part part = request.getPart("photo");//写真画像取得
+
+	String name = fL.getFileName(part);//ファイル名取得
+
+	String absolutePass = fL.setAbsolutePass(name, user.getFamilyId());//絶対パス
+
+	//フォルダのパスだけ作成
+	File target = new File("C:/pleiades/workspace/A3/WebContent/upload/family_" + user.getFamilyId() + "/");
+
+	if (!target.exists()) {//フォルダが存在しなければ作成
+		target.mkdirs();
+		//ファイル保存
+		part.write(absolutePass);
+	} else {
+		part.write(absolutePass);
 	}
+	//アイコン画像の相対パス作成
+	String relativePath = fL.setRelativePath(name, user.getFamilyId());
+	}
+
 
 }
