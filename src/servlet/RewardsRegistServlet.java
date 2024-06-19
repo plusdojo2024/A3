965deace52a7a9f5f77eca98ce78476828e8209c
plusdojo2024-chiba <chiba-riho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 17:03:35 +0900

    Update RewardsRegistServlet.java

--- src/servlet/RewardsRegistServlet.java
+++ src/servlet/RewardsRegistServlet.java
@@ -16,23 +16,13 @@ import javax.servlet.http.HttpServletResponse;
 public class RewardsRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/rewardsRegist.jsp");
+	    dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-
-
-	    request.setCharacterEncoding("UTF-8");
 
 		request.setCharacterEncoding("UTF-8");
 
