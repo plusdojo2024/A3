ea1a6d6dc4cb6557c201021fe3ac8f9c2082f862
plusdojo2024-chiba <chiba-riho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 17:03:50 +0900

    Update Rewards.java

--- src/model/Rewards.java
+++ src/model/Rewards.java
@@ -4,4 +4,102 @@ import java.io.Serializable;
 
 public class Rewards implements Serializable{
 
+	private int rewardId;  //識別番号
+	private String reward;  //ご褒美名
+	private int reqPoint;  //必要ポイント
+	private String rewardDate;  //日付
+	private int uid;  //個人ID
+	private int request;  //リクエスト中
+
+	//空のコンストラクタ
+	public Rewards() {
+
+	}
+
+	//コンストラクタ
+
+	public Rewards(int rewardId, String reward, int reqPoint, String rewardDate, int uid, int request) {
+		this.rewardId = rewardId;
+		this.reward = reward;
+		this.reqPoint = reqPoint;
+		this.rewardDate = rewardDate;
+		this.uid = uid;
+		this.request = request;
+	}
+
+
+	//getter,setter
+
+	public int getRewardId() {
+		return rewardId;
+	}
+
+	public void setRewardId(int rewardId) {
+		this.rewardId = rewardId;
+	}
+
+	public String getReward() {
+		return reward;
+	}
+
+	public void setReward(String reward) {
+		this.reward = reward;
+	}
+
+	public int getReqPoint() {
+		return reqPoint;
+	}
+
+	public void setReqPoint(int reqPoint) {
+		this.reqPoint = reqPoint;
+	}
+
+	public String getRewardDate() {
+		return rewardDate;
+	}
+
+	public void setRewardDate(String rewardDate) {
+		this.rewardDate = rewardDate;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public int getRequest() {
+		return request;
+	}
+
+	public void setRequest(int request) {
+		this.request = request;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
