6b35025b5669f9c107c00a30e5a4e6f49c36f2fd
plusdojo2024-chiba <chiba-riho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-22 02:25:53 +0900

    一旦

--- src/dao/RewardsDAO.java
+++ src/dao/RewardsDAO.java
@@ -14,6 +14,87 @@ import model.Rewards;
 
 public class RewardsDAO {
 
+	public ArrayList<Rewards> allSelect(int familyId, int role, int userId) {
+		Connection conn = null;
+		ArrayList<Rewards> list = new ArrayList<Rewards>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// １．データベースに接続する準備
+			//どのデータベースにつなぐか、つなぐためのidとpwは何かをconに入れる
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A3", "sa", " ");
+
+			// ２．SQL文を準備する（rewardsとusersを繋げる）
+			String sql = "SELECT "
+					+ "R.REWARD_ID, "
+					+ "R.REWARD, "
+					+ "R.REQ_POINT, "
+					+ "R.REWARD_DATE, "
+					+ "R.UID, "
+					+ "R.REQUEST, "
+					+ "U.NAME "
+					+ "FROM REWARDS as R "
+					+ "JOIN USERS as U ON U.UID = R.UID "
+					+ "WHERE U.FAMILY_ID = ? ";
+					if(role==0) {
+						sql+= "and R.UID = ? ";
+					}
+			//１と２の情報をpStmtに入れる
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+
+			// ?の部分が中途半端なのでSQL文を完成させる
+			pStmt.setInt(1, familyId);
+			if(role==0) {
+				pStmt.setInt(2, userId);
+			}
+			// SQL文を実行する
+			ResultSet rs = pStmt.executeQuery();
+
+			//rsの中身をArrayListに移し替える
+			while (rs.next()) {
+				//空のBeans
+				Rewards r = new Rewards();
+				//セッターを使って、中にデータベースから取ってきた値を入れていく
+				r.setRewardId(rs.getInt("REWARD_ID"));
+			    r.setReward(rs.getString("REWARD"));
+				r.setReqPoint(rs.getInt("REQ_POINT"));
+				r.setRewardDate(rs.getString("REWARD_DATE"));
+				r.setUid(rs.getInt("UID"));
+				r.setRequest(rs.getInt("REQUEST"));
+				r.setName(rs.getString("NAME"));
+
+				//全部の値が入ったらArrayListに入れる
+				list.add(r);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			list = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			list = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					list = null;
+				}
+			}
+		}
+
+		//ArrayListをServletへ返す
+		return list;
+	}
+
 	public ArrayList<Rewards> select(int rewardId, String reward, int reqPoint, String rewardDate, int uid, int request) {
 		Connection conn = null;
 		ArrayList<Rewards> list = new ArrayList<Rewards>();
@@ -117,7 +198,7 @@ public class RewardsDAO {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A3", "sa", " ");
 
 			// ２．SQL文を準備する
-			String sql = "INSERT INTO REWARDS (REWARD, REQ_POINT,UID, reward_date) VALUES (?, ?, ?);";
+			String sql = "INSERT INTO REWARDS (REWARD, REQ_POINT,UID, reward_date) VALUES (?, ?, ?, ?);";
 
 			//１と２の情報をpStmtに入れる
 			PreparedStatement pStmt = conn.prepareStatement(sql);
@@ -158,6 +239,63 @@ public class RewardsDAO {
 		return result;
 	}
 
+
+	//更新するメソッドを作りたい
+	public int update(int rewardId, String reward, int reqPoint, String rewardDate, int uid, int request) {
+		Connection conn = null;
+		int result = 0;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// １．データベースに接続する準備
+			//どのデータベースにつなぐか、つなぐためのidとpwは何かをconに入れる
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A3", "sa", " ");
+
+			// ２．SQL文を準備する
+			String sql = "UPDATE Rewards SET reward=?, req_point=?, uid=?, reward_date=?, request=? WHERE reward_Id=?";
+
+			//１と２の情報をpStmtに入れる
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//現在の日付を取ってくる
+			TimeLogic tl = new TimeLogic();
+			String now = tl.nowJpDay();
+
+			//SQL文を完成させる
+			pStmt.setString(1, reward);
+			pStmt.setInt(2, reqPoint);
+			pStmt.setInt(3, uid);
+			pStmt.setString(4, now);
+			pStmt.setInt(5, request);
+			pStmt.setInt(6, rewardId);
+
+			// SQL文を実行する（insertは登録した件数が返ってくる）
+			result = pStmt.executeUpdate();
+
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return result;
+	}
 
 
 }
