63337ec7f0f6d7b13ce2af80ce0a3435911cd475
plusdojo2024-sudo <sudo-hirohisa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-23 12:45:37 +0900

    アルバム用のソートを昇順から降順に変更

--- src/dao/NotesDAO.java
+++ src/dao/NotesDAO.java
@@ -105,7 +105,7 @@ public class NotesDAO {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A3", "sa", " ");
 
 			// SQL文を準備する
-			String sql = "SELECT * FROM Notes WHERE family_id = ? ORDER BY note_date ASC";
+			String sql = "SELECT * FROM Notes WHERE family_id = ? ORDER BY note_date DESC";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
