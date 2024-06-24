7cd451fac74251de1d6568db9ad3738b49308942
plusdojo2024-chiba <chiba-riho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-24 09:13:43 +0900

    rewardsサーブレット一旦

--- src/servlet/RewardsServlet.java
+++ src/servlet/RewardsServlet.java
@@ -23,22 +23,25 @@ public class RewardsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
 		//sessionを使いますよという宣言
 		HttpSession session = request.getSession();
+		//sessionからuser情報を持ってくる
 		Users user = (Users)session.getAttribute("dbUser");
 		int familyId = user.getFamilyId();
 		int role = user.getRole();
 		int userId = user.getUid();
 
-
+        //allSelectメソッドを使って一覧表示のためにrewardsの情報を全て持ってくる
 		RewardsDAO dao = new RewardsDAO();
+		//ログインユーザーに合わせたご褒美情報を取ってくる
 		ArrayList<Rewards> rewardsList = dao.allSelect(familyId, role, userId);
 
+		//情報を入れたRewardをセット(jspでこの情報を全て表示)
 		request.setAttribute("rewardsList", rewardsList);
 
-
-
-
+        //準備出来たらjspに飛ばす
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/rewardsAdult.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -47,11 +50,6 @@ public class RewardsServlet extends HttpServlet {
 		doGet(request, response);
 
 		request.setCharacterEncoding("UTF-8");
-
-
-
-
-
 
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/rewardsAdult.jsp");
